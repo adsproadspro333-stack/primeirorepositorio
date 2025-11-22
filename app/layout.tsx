5c171ca1c397038560572c77e86f48aa6c17c438
adsproadspro333 <adsproadspro333@gmail.com>
2025-11-22 00:27:03 -0300

    feat: adiciona Meta Pixel global no layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type React from "react"
 import type { Metadata } from "next"
+import Script from "next/script"
 import { Public_Sans } from "next/font/google"
 import { AppRouterCacheProvider } from "@mui/material-nextjs/v14-appRouter"
 import { ThemeProvider } from "@mui/material/styles"
@@ -21,6 +22,8 @@ export const metadata: Metadata = {
   generator: "v0.app",
 }
 
+const FB_PIXEL_ID = "1162544758809572"
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -28,7 +31,40 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-BR">
+      <head>
+        {/* Meta Pixel Code */}
+        <Script
+          id="fb-pixel"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)_fbq=n;n.push=n; n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '${FB_PIXEL_ID}');
+fbq('track', 'PageView');
+          `,
+          }}
+        />
+        {/* End Meta Pixel Code */}
+      </head>
       <body className={`${publicSans.className} with-sticky-header`}>
+        {/* NoScript para quando o JS estiver desativado */}
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src={`https://www.facebook.com/tr?id=${FB_PIXEL_ID}&ev=PageView&noscript=1`}
+            alt=""
+          />
+        </noscript>
+
         <AppRouterCacheProvider>
           <ThemeProvider theme={theme}>
             <CssBaseline />
